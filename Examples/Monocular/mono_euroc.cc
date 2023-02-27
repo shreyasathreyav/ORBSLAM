d4cd08a0f8a4a2738f46bb85dc2ce3a37ce01738
shreyas <shreyasathreyav@gmail.com>
2023-02-27 18:46:06 -0500

    commit with identifier

--- Examples/Monocular/mono_euroc.cc
+++ Examples/Monocular/mono_euroc.cc
@@ -1,4 +1,5 @@
 /**
+ * This is our first commit
 * This file is part of ORB-SLAM3
 *
 * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
