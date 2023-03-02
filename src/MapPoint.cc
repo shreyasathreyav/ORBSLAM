01ed7cd4cd16168c215d66b7180076003d132577
shreyas <shreyasathreyav@gmail.com>
2023-03-02 16:48:35 -0500

    Last known good point

--- src/MapPoint.cc
+++ src/MapPoint.cc
@@ -12,7 +12,7 @@
 * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3
 * If not, see <http://www.gnu.org/licenses/>.
 */
 
