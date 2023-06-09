833db296d62e1a292d733362fd88daf49f153b43
zhartov <magusshulem@gmail.com>
2023-06-09 19:45:49 -0400

    Added results

--- automate_with_sed.sh
+++ automate_with_sed.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-mkdir results_deletion_V1_02
-cd results_deletion_V1_02
+# mkdir results_deletion_V1_02
+# cd results_deletion_V1_02
 
-tester="result_file"
-for i in {1..10}
-do
-	tester+="$i.txt"
-	touch $tester
+# tester="result_file"
+# for i in {1..10}
+# do
+# 	tester+="$i.txt"
+# 	touch $tester
 
-        ../Examples/Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ../Examples/Monocular/EuRoC.yaml /home/nitin/Downloads/Datasets_ORBSLAM/vicon_room1/V1_02_medium ../Examples/Monocular/EuRoC_TimeStamps/V102.txt dataset-V102_mono >> $tester 2>&1 
-	sed -i -e :a -e '$q;N;12,$D;ba' $tester	
-	tester="result_file"
-done
+#         ../Examples/Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ../Examples/Monocular/EuRoC.yaml /home/nitin/Downloads/Datasets_ORBSLAM/vicon_room1/V1_02_medium ../Examples/Monocular/EuRoC_TimeStamps/V102.txt dataset-V102_mono >> $tester 2>&1 
+# 	tester="result_file"
+# done
 
-cd ..
+# cd ..
 
 mkdir results_deletion_V1_03
 cd results_deletion_V1_03
@@ -26,7 +25,7 @@ do
 	touch $tester
 
         ../Examples/Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ../Examples/Monocular/EuRoC.yaml /home/nitin/Downloads/Datasets_ORBSLAM/vicon_room1/V1_03_difficult ../Examples/Monocular/EuRoC_TimeStamps/V103.txt dataset-V103_mono >> $tester 2>&1 
-	sed -i -e :a -e '$q;N;12,$D;ba' $tester	
+	# sed -i -e :a -e '$q;N;50,$D;ba' $tester	
 	tester="result_file"
 done
 
@@ -42,7 +41,7 @@ do
 	touch $tester
 
         ../Examples/Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ../Examples/Monocular/EuRoC.yaml /home/nitin/Downloads/Datasets_ORBSLAM/vicon_room2/V2_01_easy ../Examples/Monocular/EuRoC_TimeStamps/V201.txt dataset-V201_mono >> $tester 2>&1 
-	sed -i -e :a -e '$q;N;12,$D;ba' $tester	
+	# sed -i -e :a -e '$q;N;50,$D;ba' $tester	
 	tester="result_file"
 done
 
@@ -58,7 +57,7 @@ do
 	touch $tester
 
         ../Examples/Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ../Examples/Monocular/EuRoC.yaml /home/nitin/Downloads/Datasets_ORBSLAM/vicon_room2/V2_02_medium ../Examples/Monocular/EuRoC_TimeStamps/V202.txt dataset-V202_mono >> $tester 2>&1 
-	sed -i -e :a -e '$q;N;12,$D;ba' $tester	
+	# sed -i -e :a -e '$q;N;50,$D;ba' $tester	
 	tester="result_file"
 done
 
@@ -74,7 +73,7 @@ do
 	touch $tester
 
         ../Examples/Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ../Examples/Monocular/EuRoC.yaml /home/nitin/Downloads/Datasets_ORBSLAM/vicon_room2/V2_03_difficult ../Examples/Monocular/EuRoC_TimeStamps/V203.txt dataset-V203_mono >> $tester 2>&1 
-	sed -i -e :a -e '$q;N;12,$D;ba' $tester	
+	# sed -i -e :a -e '$q;N;50,$D;ba' $tester	
 	tester="result_file"
 done
 
