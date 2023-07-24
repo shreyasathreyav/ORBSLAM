27a11a486660dbf31ad73aa02d81c237efa4b740
Ubuntu <ubuntu@ip-172-31-17-106.ec2.internal>
2023-07-24 21:15:29 +0000

    New

--- Examples/Monocular/mono_euroc.cc
+++ Examples/Monocular/mono_euroc.cc
@@ -227,3 +227,14 @@ void LoadImages(const string &strImagePath, const string &strPathTimes,
         }
     }
 }
+void* operator new(std :: size_t count)
+{
+auto ptr = malloc(count);
+TracyAlloc (ptr , count);
+return ptr;
+}
+void operator delete(void* ptr) noexcept
+{
+TracyFree (ptr);
+free(ptr);
+}
